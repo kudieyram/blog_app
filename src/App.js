887187f1cf6fd3ef4112963fb67c1737ef029997
kudi_eyram <noraeyram.kudiabor@gmail.com>
2020-11-19 09:14:53 +0000

    stlying of the Login and Signup pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import LogInComponent from './components/LogIn';
+import SignUpComponent from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-        <LogInComponent/>
+        <SignUpComponent/>
     </div>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,19 +7,23 @@ function SignUpComponent() {
     <div>
         <div className="login-form">
     <form>
-        <h2 className="text-center">Log in</h2>       
+        <h2 className="text-center">Sign up</h2>       
+        <div className="form-group">
+            <input type="text" className="form-control" placeholder="Full Name" required="required"/>
+        </div>
         <div className="form-group">
             <input type="email" className="form-control" placeholder="Email" required="required"/>
         </div>
         <div className="form-group">
             <input type="password" className="form-control" placeholder="Password" required="required"/>
         </div>
+
         <div className="form-group">
-            <button type="submit" className="btn btn-primary btn-block">Log in</button>
+            <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
         </div>
               
     </form>
-    <p className="text-center"><a href="#">Create an Account</a></p>
+    <p className="text-center">Already have an account<a href="#"> Log in</a></p>
 </div>
     </div>
   );
