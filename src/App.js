286852ac42810e09d8a9de8c25ac717c27074d51
kudi_eyram <noraeyram.kudiabor@gmail.com>
2020-11-19 09:29:21 +0000

    added route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import LogInComponent from './components/LogIn';
 import SignUpComponent from './components/SignUp';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+
+function App() { 
   return (
-    <div className="App">
-        <SignUpComponent/>
-    </div>
+    <Router>
+      <div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <LogInComponent/>
+          </Route>
+          <Route path="/signup">
+            <SignUpComponent />
+          </Route>
+          <Route path="/">
+            <LogInComponent/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
